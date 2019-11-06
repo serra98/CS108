bebd1159ea45a7a518573dc3478ee6b1451a70e8
serra <serra.jung@gmail.com>
2019-11-06 16:31:08 -0500

    modified hello world page.

--- pages/views.py
+++ pages/views.py
@@ -8,4 +8,19 @@ from django.http import HttpResponse # create a response to a URL request
 def homePageView(request):
     ''' This function will respond to an HTTP request and return an HttpRequest object.'''
 
-    return HttpResponse('Hello, world!')
+  
+
+    s = '''
+    <html>
+    <head> 
+        <title>Hello,world!</title>
+    </head>
+    <body>
+    <h1>Hello,world! </h1>
+    Welcome to our first Django web application! 
+    <p> 
+
+    </body>
+    </html>
+    '''
+      return HttpResponse('Hello, world!')
