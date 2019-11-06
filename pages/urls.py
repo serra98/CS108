ff25ca24a1a13ba358500ee7bec977c9888b301d
serra <serra.jung@gmail.com>
2019-11-06 16:46:28 -0500

    1

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 # pages/urls.py
 
 from django.urls import path 
-from .views import homePageView # our response function 
+from .views import HomePageView # our class definition
 
 urlpatterns = [
     #map the URL to the function homePageView
-    path('', homePageView, name = 'home')
+    #path('', homePageView, name = 'home') #function-based view 
+    path('',HomePageView.as_view(), name = 'home') #generic class - base view 
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,26 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse # create a response to a URL request 
-
+from django.views.generic import TemplateView #base class for a generic view 
 
 
 # Create your views here.
 
-def homePageView(request):
-    ''' This function will respond to an HTTP request and return an HttpRequest object.'''
+class homePageView(TemplateView):
+    '''Inherit from the generic TemplateView to use an external HTML template '''
+    template_name = 'pages/home.html'
+    
+#def homePageView(request):
+   # ''' This function will respond to an HTTP request and return an HttpRequest object.'''
 
   
 
-    s = '''
-<html>
-<head> 
-    <title>Hello,world!</title>
-</head>
-<body>
-<h1>Hello,world! </h1>
-Welcome to our first Django web application! 
-<p> 
+  #  s = '''
+#<html>
+#<head> 
+ #   <title>Hello,world!</title>
+#</head>
+#<body>
+#<h1>Hello,world! </h1>
+#Welcome to our first Django web application! 
+#<p> 
 
-</body>
-</html>
-    '''
-    return HttpResponse(s)
+#</body>
+#</html>
+  #  '''
+ #   return HttpResponse(s)
